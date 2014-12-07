ebdb43a8d6367cfcd0f04202d29a7a32ed5b15cc
xmichaelx <michal.drzal@gmail.com>
2014-12-07 01:38:05 +0100

    Added knockout to enhance readability

--- dla/simulation_setup.js
+++ dla/simulation_setup.js
@@ -1,58 +1,57 @@
 var intervalId = -1;
 
-function stopSimulation() {
-	if (intervalId > -1) {
-		clearInterval(intervalId);
-	}
-}
-
-function startSimulation() {
-	var cond = document.querySelector('input[name="optradio"]:checked').value;
-
-
-    var density = parseFloat(document.getElementById("particleDensity").value);
-    var width = parseInt(document.getElementById("canvasWidth").value);
-    var height = parseInt(document.getElementById("canvasHeight").value);
-    var particleCount = Math.floor(width * height * density);
-    var stoppingFraction = parseFloat(document.getElementById("stoppingFraction").value);
-
-
-
-	var canvas = resizeCanvas(width, height);
-	var field = createField(width, height);
-	var particles = createParticles(field, particleCount);
-
-	condensation[cond](field);
-	resetAllParticles(particles,field);
-
-
-	if (intervalId > -1) {
-		clearInterval(intervalId);
-	}
-
-	intervalId = setInterval(function() {
-		for (var i = 0;i < 4;i++) {
-			updateAllParticles(particles, field);
+function SimulationViewModel () {
+	this.stopSimulation = function () {
+		if (intervalId > -1) {
+			clearInterval(intervalId);
 		}
-		
-		paint(field, canvas, particles, "FF0000","00FF00");
+	};
 
-		var idle = 0;
-		for (var i =0;i<particles.count;i++) {
-			idle += particles.stuck[i];
+	this.startSimulation = function() {
+		var cond = document.querySelector('input[name="optradio"]:checked').value;
+		var canvas = resizeCanvas(this.canvasWidth(), this.canvasHeight());
+		var field = createField(this.canvasWidth(), this.canvasHeight());
+
+		var particleCount = Math.floor(this.canvasWidth() * this.canvasHeight() * this.density());
+		var particles = createParticles(field, particleCount);
+
+		condensation[cond](field);
+		resetAllParticles(particles,field);
+
+		if (intervalId > -1) {
+			clearInterval(intervalId);
 		}
 
-		if (idle > stoppingFraction * particles.count) {
-			stopSimulation();
-			paint(field, canvas, particles, "FF000FF","00FFFF");
+		var stopSimulation = this.stopSimulation;
+		var stoppingFraction = this.stoppingfraction();
 
-		}
+		intervalId = setInterval(function() {
+			for (var i = 0;i < 4;i++) {
+				updateAllParticles(particles, field);
+			}
+			
+			paint(field, canvas, particles, "FF0000","00FF00");
 
-	} , 33);
-}
+			var idle = 0;
+			var i = particles.count;
+			while (i--) {
+				idle += particles.stuck[i];
+			}
 
+			if (idle > stoppingFraction * particles.count) {
+				stopSimulation();
+				paint(field, canvas, particles, "FF000FF","00FFFF");
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("start_simulation").onclick = startSimulation;
-    document.getElementById("stop_simulation").onclick = stopSimulation;
-});
+			}
+
+		} , 33);
+	};
+
+	this.canvasWidth = ko.observable(500);
+	this.canvasHeight = ko.observable(500);
+	this.density = ko.observable(0.15);
+	this.stoppingfraction = ko.observable(0.25);
+
+};
+
+ko.applyBindings(new SimulationViewModel());
